228cecbcd69a80b555143467261c1178ca110158
duarte-arthur21 <ferreiraduarte21@gmail.com>
2024-12-05 17:18:18 -0300

    Adicionando Classes Dinamicamente e Mapeando teclas permitidas

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -1,16 +1,28 @@
 const pianoKeys = document.querySelectorAll(".piano-keys .key");
 
+let mapedkeys = [];
 let audio = new Audio("src/tunes/a.wav");
 
 const playTune = (key) => {
     audio.src = `src/tunes/${key}.wav`;
     audio.play();
+
+    const clickedkey = document.querySelector(`[data-key="${key}"]`)
+    clickedkey.classList.add("active");
+    setTimeout(() => {
+        clickedkey.classList.remove("active");
+    }, 150);
 };
 
 pianoKeys.forEach((key) =>{
     key.addEventListener("click", () => playTune(key.dataset.key));
+    mapedkeys.push(key.dataset.key);
+
 });
 
 document.addEventListener("keydown", (e) => {
+    
+    if(mapedkeys.includes(e.key)){
     playTune(e.key);
+    }
 });
