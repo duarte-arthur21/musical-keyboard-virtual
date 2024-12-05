d21900e2e787af5690472bcd6657334e69be2738
duarte-arthur21 <ferreiraduarte21@gmail.com>
2024-12-05 17:51:53 -0300

    Trabalhando com Toggle

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -1,5 +1,6 @@
 const pianoKeys = document.querySelectorAll(".piano-keys .key");
-const volumeSlider = document.querySelector(".volume-slider input")
+const volumeSlider = document.querySelector(".volume-slider input");
+const keysCheck = document.querySelector(".keys-check input");
 
 let mapedkeys = [];
 let audio = new Audio("src/tunes/a.wav");
@@ -30,4 +31,10 @@ const handleVolume = (e) => {
     audio.volume = e.target.value;
 }
 
+const showHidekeys = () => {
+    pianoKeys.forEach((key) => key.classList.toggle("hide"));
+}
+
 volumeSlider.addEventListener("input", handleVolume);
+
+keysCheck.addEventListener("click", showHidekeys);
